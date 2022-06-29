afcba20e2d4858ab289652a4dcf0ad31fd306ece
Alex Williams <alexwilliams@protonmail.ch>
2022-06-29 00:06:33 +0000

    added flashlight code and version number

---
+++ fl-minimal/flashlight/flashlight/fl/optim/optim.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "flashlight/fl/optim/Optimizers.h"
+#include "flashlight/fl/optim/SGDOptimizer.h"
+#include "flashlight/fl/optim/Utils.h"
