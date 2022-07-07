78ec4daf1fe914deb46c011bbc89a6ab41e18356
Alex Williams <alexwilliams@protonmail.ch>
2022-07-07 05:02:30 +0000

    updated build.sh

--- fl-minimal/build.sh
+++ fl-minimal/build.sh
@@ -10,6 +10,7 @@ mkdir -p $BUILD_DIR
 
 cmake .. -DCMAKE_BUILD_TYPE=Release \
 	-DFL_BUILD_ARRAYFIRE=ON \
+  -DFL_ARRAYFIRE_USE_CUDA=ON \
 	-DFL_BUILD_TESTS=ON \
 	-DCMAKE_CUDA_COMPILER='/usr/local/cuda/bin/nvcc' \
 	-B $BUILD_DIR \
