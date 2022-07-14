e9ff2f0b4980abe9784b365d51d46c3c2d1bee0b
Alex Williams <alexwilliams@protonmail.ch>
2022-07-14 19:59:38 +0000

    removed inclusion of distributed.h

--- fl-minimal/flashlight/flashlight/fl/flashlight.h
+++ fl-minimal/flashlight/flashlight/fl/flashlight.h
@@ -10,7 +10,6 @@
 #include "flashlight/fl/autograd/autograd.h"
 #include "flashlight/fl/common/common.h"
 #include "flashlight/fl/dataset/datasets.h"
-#include "flashlight/fl/distributed/distributed.h"
 #include "flashlight/fl/meter/meters.h"
 #include "flashlight/fl/nn/nn.h"
 #include "flashlight/fl/optim/optim.h"
